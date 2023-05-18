08ec2a950375f030d9f4077833fd44bbb939c022
Justin Stowe <justinstowe12@gmail.com>
2023-05-18 11:40:33 -0600

    submenu stays open until cursor leaves menu as a whole.

--- javaScript.js
+++ javaScript.js
@@ -1,15 +1,22 @@
-function menuItemEnter() {
+function showSubmenu() {
   const submenu = document.getElementsByClassName("menu__sub")[0];
   submenu.style.display = "block";
 }
 
-function menuItemLeave() {
+function hideSubmenu() {
   const submenu = document.getElementsByClassName("menu__sub")[0];
   submenu.style.display = "none";
 }
 
+function onMenuItemMouseEnter(item) {
+  item.classList.add("menu__main__item--active");
+  showSubmenu();
+}
+
 const menuItems = document.getElementsByClassName("menu__main__item");
 for (const menuItem of menuItems) {
-  menuItem.onmouseenter = menuItemEnter;
-  menuItem.onmouseleave = menuItemLeave;
+  menuItem.onmouseenter = () => onMenuItemMouseEnter(menuItem);
 }
+
+const menu = document.getElementsByClassName("menu")[0];
+menu.onmouseleave = hideSubmenu;
